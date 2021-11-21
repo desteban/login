4314d1043e1f14e35b60616e597fd9dc80d84fe8
desteban <desteban28c@hotmail.com>
2021-11-21 13:02:27 -0500

    fuentes y utilizar normalize.css

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import header from "next/head";
+
+class Login extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <title>Login</title>
+        </header>
+
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
