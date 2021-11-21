58e4e578976508e548a551fb256183823f8bb6db
desteban <desteban28c@hotmail.com>
2021-11-21 13:20:49 -0500

    paleta de colores

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,33 @@
-import { Component } from "react";
-import header from "next/head";
+import { Component } from 'react';
+import header from 'next/head';
 
 class Login extends Component {
-  constructor(props: any) {
-    super(props);
-  }
+	constructor(props: any) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div>
-        <header>
-          <title>Login</title>
-        </header>
+	render() {
+		return (
+			<div>
+				<header>
+					<title>Login</title>
+				</header>
 
-        <h1>Login</h1>
-      </div>
-    );
-  }
+				<h1>Login</h1>
+				<h2>Login</h2>
+				<h3>Login</h3>
+				<h4>Login</h4>
+				<h5>Login</h5>
+				<h6>Login</h6>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim id cum quibusdam
+					cupiditate minus voluptate, aliquid laboriosam itaque facilis, error laudantium
+					neque. Voluptatem eum ipsa, expedita hic illo sapiente quasi?
+				</p>
+			</div>
+		);
+	}
 }
 
 export default Login;
